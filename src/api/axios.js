921388f4b1c0563a08b39b2ba949b86fdd280d19
vanshgoel123 <vanshg247776@gmail.com>
2025-12-06 02:15:37 +0530

    remove env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const BASE_URL = "http://172.16.61.173:8000/api/v1";
 const api = axios.create({
-    baseURL: import.meta.env.BASE_URL,
+    baseURL:BASE_URL,
     withCredentials: true, // ye cookies  include karwata hai request mei
 });
 
