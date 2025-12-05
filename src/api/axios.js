4a0caaf3b5b37c9edfc606b44a658fe3e65fffc7
vanshgoel123 <vanshg247776@gmail.com>
2025-12-06 03:21:13 +0530

    Fix API base URL for production deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-const BASE_URL = "/api/v1";
 const api = axios.create({
-    baseURL:BASE_URL,
-    withCredentials: true, // ye cookies  include karwata hai request mei
+  baseURL: "/api/v1",
+  withCredentials: true,
 });
 
 export default api
