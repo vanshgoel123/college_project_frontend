dfe074e9b8234052c01419719b6af46ac6d2b796
vanshgoel123 <vanshg247776@gmail.com>
2025-12-12 14:14:38 +0530

    name

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -26,7 +26,7 @@ export default function Sidebar() {
             <div>
                 <div className="h-16 flex items-center px-6 border-b border-gray-100 dark:border-gray-800">
            <span className="text-xl font-bold text-gray-800 dark:text-gray-100 tracking-tight">
-             <span className="text-blue-600 dark:text-blue-400">Research</span>OS
+             <span className="text-blue-600 dark:text-blue-400">IIIT</span>NR
            </span>
                 </div>
 
